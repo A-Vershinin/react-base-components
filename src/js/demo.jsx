2d8f25e90e65e4328478851227be804b7b692311
Anatoliy Vershinin <cata07@yandex.ru>
2017-11-24 22:11:54 +0200

    start react project

---
+++ src/js/demo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class DemoProjects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+      return (
+        <div>Hello React</div>
+        );
+    }
+}
+
+export default DemoProjects;
